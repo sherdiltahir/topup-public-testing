52ce93a29a9c34e6759384197110b64b99994091
mubeen-ikram <mubeen.ikram1999@gmail.com>
2020-10-03 15:04:20 +0500

    Some minor changes

--- lib/ModelClasses/UserModel.dart
+++ lib/ModelClasses/UserModel.dart
@@ -1,55 +1,55 @@
-import 'dart:io';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class User{
-  String name='';
-  String id= '';
-  String phoneNumber='';
-  List<dynamic> cards=new List<dynamic>();
-  List<dynamic> couponsIds=new List<dynamic>();
-  String fcmDeviceCode='';
-  File picture;
-  String pictureUri;
-  String scurityPin;
-  String securityQuestion1;
-  String securityAnswer1;
-
-
-  Map<String, dynamic>  toMap() {
-    return {
-      'name': name,
-      'id': id,
-      'phone': phoneNumber,
-      'card': cards,
-      'couponsIds': couponsIds,
-      'fcmDeviceCode': fcmDeviceCode,
-      'pictureUri': pictureUri,
-      'scurityPin': scurityPin,
-      'securityQuestion1': securityQuestion1,
-      'securityAnswer1': securityAnswer1,
-    };
-  }
-
-
-
-
-  User({this.name, this.id, this.email, this.phone, this.shipments, this.review,
-    this.location, this.fcmDeviceId});
-
-  void  fromMap(DocumentSnapshot doc) {
-    this.name = doc.data['name'] ?? '';
-    this.id = doc.data['id'];
-    this.fcmDeviceId = doc.data['fcmDeviceId'];
-    this.phone = doc.data['phone'] ?? '';
-    this.email = doc.data['email'] ?? '';
-    if (doc.data['shipments'] != null)
-      this.shipments = List.castFrom(doc.data['shipments'] ?? '');
-    else{
-      this.shipments=new List();
-    }
-    this.review = doc.data['review'];
-    this.location = doc.data['location'] ?? '';
-  }
-
-}
+// import 'dart:io';
+//
+// import 'package:cloud_firestore/cloud_firestore.dart';
+//
+// class User{
+//   String name='';
+//   String id= '';
+//   String phoneNumber='';
+//   List<dynamic> cards=new List<dynamic>();
+//   List<dynamic> couponsIds=new List<dynamic>();
+//   String fcmDeviceCode='';
+//   File picture;
+//   String pictureUri;
+//   String scurityPin;
+//   String securityQuestion1;
+//   String securityAnswer1;
+//
+//
+//   Map<String, dynamic>  toMap() {
+//     return {
+//       'name': name,
+//       'id': id,
+//       'phone': phoneNumber,
+//       'card': cards,
+//       'couponsIds': couponsIds,
+//       'fcmDeviceCode': fcmDeviceCode,
+//       'pictureUri': pictureUri,
+//       'scurityPin': scurityPin,
+//       'securityQuestion1': securityQuestion1,
+//       'securityAnswer1': securityAnswer1,
+//     };
+//   }
+//
+//
+//
+//
+//   User({this.name, this.id, this.email, this.phone, this.shipments, this.review,
+//     this.location, this.fcmDeviceId});
+//
+//   void  fromMap(DocumentSnapshot doc) {
+//     this.name = doc.data['name'] ?? '';
+//     this.id = doc.data['id'];
+//     this.fcmDeviceId = doc.data['fcmDeviceId'];
+//     this.phone = doc.data['phone'] ?? '';
+//     this.email = doc.data['email'] ?? '';
+//     if (doc.data['shipments'] != null)
+//       this.shipments = List.castFrom(doc.data['shipments'] ?? '');
+//     else{
+//       this.shipments=new List();
+//     }
+//     this.review = doc.data['review'];
+//     this.location = doc.data['location'] ?? '';
+//   }
+//
+// }
--- lib/Services/FirebaseDatabaseService.dart
+++ lib/Services/FirebaseDatabaseService.dart
@@ -1,139 +1,139 @@
-
-class DatabaseService {
-  final String uid;
-  String otherUid;
-  String shipmentId;
-
-  DatabaseService(this.uid);
-
-  // collection reference
-  final CollectionReference userCollection =
-  Firestore.instance.collection('users');
-  // collection reference
-  final CollectionReference driverCollection =
-  Firestore.instance.collection('Drivers');
-  final CollectionReference shipmentCollection =
-  Firestore.instance.collection('shipments');
-
-  Future<void> registerShipmentToUserList(Shipment currentShipment) async {
-    await setShipmentData(currentShipment);
-    await userCollection.document(this.uid).get().then((querySnapshot) {
-      User currentUser = _userDataFromSnapshot(querySnapshot);
-      if(currentUser.shipments== null)
-        currentUser.shipments=new List<String>();
-      currentUser.shipments.add(currentShipment.shipmentId);
-      userCollection.document(uid).updateData({
-        'shipments': currentUser.shipments
-      });
-    });
-  }
-
-  Future<void> setUserData(
-      User user) async {
-    return await userCollection.document(uid).setData(user.toMap());
-  }
-
-  Future<void> updateShipmentData(
-      Shipment shipment) async {
-    return await shipmentCollection.document(shipment.shipmentId).setData(shipment.toMap());
-  }
-
-
-
-
-
-
-  // brew list from snapshot
-
-  List<Driver> _driverListFromSnapshot(QuerySnapshot snapshot) {
-    return snapshot.documents.map((doc) {
-      return _driverDataFromSnapshot(doc);
-    }).toList();
-  }
-
-
-  List<Shipment> _shipmentListFromSnapshot(QuerySnapshot snapshot) {
-    return snapshot.documents.map((doc) {
-      return _shipmentDataFromSnapshot(doc);
-    }).toList();
-  }
-
-  List<User> _userListFromSnapshot(QuerySnapshot snapshot) {
-    return snapshot.documents.map((snapshot) {
-      return _userDataFromSnapshot(snapshot);
-    }).toList();
-  }
-
-  // user data from snapshots
-  Driver _driverDataFromSnapshot(DocumentSnapshot snapshot) {
-    Driver myDriver=Driver();
-    myDriver.fromMap(snapshot);
-    return myDriver;
-  }
-
-
-  User _userDataFromSnapshot(DocumentSnapshot snapshot) {
-    User newUser=User();
-    newUser.fromMap(snapshot);
-    return newUser;
-  }
-
-  Shipment _shipmentDataFromSnapshot(DocumentSnapshot doc) {
-    Shipment current=Shipment();
-    current.fromMap(doc);
-    return current;
-  }
-
-  Stream<List<Driver>> get drivers {
-    return driverCollection.snapshots().map(_driverListFromSnapshot);
-  }
-  // get brews stream
-  Stream<List<Shipment>> get shipments {
-    return shipmentCollection.snapshots().map(_shipmentListFromSnapshot);
-  }
-
-  Stream<List<Shipment>> get userShipments {
-    return shipmentCollection
-        .where("customerId", isEqualTo: uid)
-        .snapshots()
-        .map(_shipmentListFromSnapshot);
-  }
-
-  Stream<List<User>> get users {
-    return shipmentCollection.snapshots().map(_userListFromSnapshot);
-  }
-
-  Stream<Shipment> getParticularShipment(String shipmentId) {
-    this.shipmentId = shipmentId;
-    return _shipment;
-  }
-
-  Stream<Driver> getParticularDriver(String userId) {
-    this.otherUid = userId;
-    return userData;
-  }
-
-  Stream<Shipment> get _shipment {
-    return shipmentCollection
-        .document(shipmentId)
-        .snapshots()
-        .map(_shipmentDataFromSnapshot);
-  }
-
-  Stream<Driver> get userData {
-    return driverCollection
-        .document(otherUid)
-        .snapshots()
-        .map(_driverDataFromSnapshot);
-  }
-
-  Stream<User> get currentUserData {
-    return userCollection.document(uid).snapshots().map(_userDataFromSnapshot);
-  }
-
-  Future setShipmentData(Shipment currentShipment) async {
-    return await shipmentCollection
-        .document(currentShipment.shipmentId)
-        .setData(currentShipment.toMap());
-  }
-}
+//
+// class DatabaseService {
+//   final String uid;
+//   String otherUid;
+//   String shipmentId;
+//
+//   DatabaseService(this.uid);
+//
+//   // collection reference
+//   final CollectionReference userCollection =
+//   Firestore.instance.collection('users');
+//   // collection reference
+//   final CollectionReference driverCollection =
+//   Firestore.instance.collection('Drivers');
+//   final CollectionReference shipmentCollection =
+//   Firestore.instance.collection('shipments');
+//
+//   Future<void> registerShipmentToUserList(Shipment currentShipment) async {
+//     await setShipmentData(currentShipment);
+//     await userCollection.document(this.uid).get().then((querySnapshot) {
+//       User currentUser = _userDataFromSnapshot(querySnapshot);
+//       if(currentUser.shipments== null)
+//         currentUser.shipments=new List<String>();
+//       currentUser.shipments.add(currentShipment.shipmentId);
+//       userCollection.document(uid).updateData({
+//         'shipments': currentUser.shipments
+//       });
+//     });
+//   }
+//
+//   Future<void> setUserData(
+//       User user) async {
+//     return await userCollection.document(uid).setData(user.toMap());
+//   }
+//
+//   Future<void> updateShipmentData(
+//       Shipment shipment) async {
+//     return await shipmentCollection.document(shipment.shipmentId).setData(shipment.toMap());
+//   }
+//
+//
+//
+//
+//
+//
+//   // brew list from snapshot
+//
+//   List<Driver> _driverListFromSnapshot(QuerySnapshot snapshot) {
+//     return snapshot.documents.map((doc) {
+//       return _driverDataFromSnapshot(doc);
+//     }).toList();
+//   }
+//
+//
+//   List<Shipment> _shipmentListFromSnapshot(QuerySnapshot snapshot) {
+//     return snapshot.documents.map((doc) {
+//       return _shipmentDataFromSnapshot(doc);
+//     }).toList();
+//   }
+//
+//   List<User> _userListFromSnapshot(QuerySnapshot snapshot) {
+//     return snapshot.documents.map((snapshot) {
+//       return _userDataFromSnapshot(snapshot);
+//     }).toList();
+//   }
+//
+//   // user data from snapshots
+//   Driver _driverDataFromSnapshot(DocumentSnapshot snapshot) {
+//     Driver myDriver=Driver();
+//     myDriver.fromMap(snapshot);
+//     return myDriver;
+//   }
+//
+//
+//   User _userDataFromSnapshot(DocumentSnapshot snapshot) {
+//     User newUser=User();
+//     newUser.fromMap(snapshot);
+//     return newUser;
+//   }
+//
+//   Shipment _shipmentDataFromSnapshot(DocumentSnapshot doc) {
+//     Shipment current=Shipment();
+//     current.fromMap(doc);
+//     return current;
+//   }
+//
+//   Stream<List<Driver>> get drivers {
+//     return driverCollection.snapshots().map(_driverListFromSnapshot);
+//   }
+//   // get brews stream
+//   Stream<List<Shipment>> get shipments {
+//     return shipmentCollection.snapshots().map(_shipmentListFromSnapshot);
+//   }
+//
+//   Stream<List<Shipment>> get userShipments {
+//     return shipmentCollection
+//         .where("customerId", isEqualTo: uid)
+//         .snapshots()
+//         .map(_shipmentListFromSnapshot);
+//   }
+//
+//   Stream<List<User>> get users {
+//     return shipmentCollection.snapshots().map(_userListFromSnapshot);
+//   }
+//
+//   Stream<Shipment> getParticularShipment(String shipmentId) {
+//     this.shipmentId = shipmentId;
+//     return _shipment;
+//   }
+//
+//   Stream<Driver> getParticularDriver(String userId) {
+//     this.otherUid = userId;
+//     return userData;
+//   }
+//
+//   Stream<Shipment> get _shipment {
+//     return shipmentCollection
+//         .document(shipmentId)
+//         .snapshots()
+//         .map(_shipmentDataFromSnapshot);
+//   }
+//
+//   Stream<Driver> get userData {
+//     return driverCollection
+//         .document(otherUid)
+//         .snapshots()
+//         .map(_driverDataFromSnapshot);
+//   }
+//
+//   Stream<User> get currentUserData {
+//     return userCollection.document(uid).snapshots().map(_userDataFromSnapshot);
+//   }
+//
+//   Future setShipmentData(Shipment currentShipment) async {
+//     return await shipmentCollection
+//         .document(currentShipment.shipmentId)
+//         .setData(currentShipment.toMap());
+//   }
+// }
