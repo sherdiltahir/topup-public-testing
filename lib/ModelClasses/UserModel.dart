5fbc84c1e20f032818f10bcf12c5a43a43d72112
mubeen-ikram <mubeen.ikram1999@gmail.com>
2020-10-03 16:30:15 +0500

    Updated User Model with Map Functionality

--- lib/ModelClasses/UserModel.dart
+++ lib/ModelClasses/UserModel.dart
@@ -1,55 +1,66 @@
-// import 'dart:io';
-//
-// import 'package:cloud_firestore/cloud_firestore.dart';
-//
-// class User{
-//   String name='';
-//   String id= '';
-//   String phoneNumber='';
-//   List<dynamic> cards=new List<dynamic>();
-//   List<dynamic> couponsIds=new List<dynamic>();
-//   String fcmDeviceCode='';
-//   File picture;
-//   String pictureUri;
-//   String scurityPin;
-//   String securityQuestion1;
-//   String securityAnswer1;
-//
-//
-//   Map<String, dynamic>  toMap() {
-//     return {
-//       'name': name,
-//       'id': id,
-//       'phone': phoneNumber,
-//       'card': cards,
-//       'couponsIds': couponsIds,
-//       'fcmDeviceCode': fcmDeviceCode,
-//       'pictureUri': pictureUri,
-//       'scurityPin': scurityPin,
-//       'securityQuestion1': securityQuestion1,
-//       'securityAnswer1': securityAnswer1,
-//     };
-//   }
-//
-//
-//
-//
-//   User({this.name, this.id, this.email, this.phone, this.shipments, this.review,
-//     this.location, this.fcmDeviceId});
-//
-//   void  fromMap(DocumentSnapshot doc) {
-//     this.name = doc.data['name'] ?? '';
-//     this.id = doc.data['id'];
-//     this.fcmDeviceId = doc.data['fcmDeviceId'];
-//     this.phone = doc.data['phone'] ?? '';
-//     this.email = doc.data['email'] ?? '';
-//     if (doc.data['shipments'] != null)
-//       this.shipments = List.castFrom(doc.data['shipments'] ?? '');
-//     else{
-//       this.shipments=new List();
-//     }
-//     this.review = doc.data['review'];
-//     this.location = doc.data['location'] ?? '';
-//   }
-//
-// }
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User{
+  String name='';
+  String id= '';
+  String phoneNumber='';
+  List<dynamic> cards=new List<dynamic>();
+  List<dynamic> couponsIds=new List<dynamic>();
+  String fcmDeviceCode='';
+  File picture;
+  String pictureUri;
+  String securityPin;
+  String securityQuestion1;
+  String securityAnswer1;
+  String securityQuestion2;
+  String securityAnswer2;
+
+
+  Map<String, dynamic>  toMap() {
+    return {
+      'name': name,
+      'id': id,
+      'phone': phoneNumber,
+      'card': cards,
+      'couponsIds': couponsIds,
+      'fcmDeviceCode': fcmDeviceCode,
+      'pictureUri': pictureUri,
+      'securityPin': securityPin,
+      'securityQuestion1': securityQuestion1,
+      'securityAnswer1': securityAnswer1,
+      'securityQuestion2': securityQuestion2,
+      'securityAnswer2': securityAnswer2,
+    };
+  }
+
+
+
+
+  User({this.name, this.id, this.phoneNumber, this.cards, this.couponsIds, this.fcmDeviceCode,
+    this.pictureUri, this.securityPin,this.securityQuestion1, this.securityAnswer1});
+
+  void  fromMap(DocumentSnapshot doc) {
+    this.name = doc.data['name'] ?? '';
+    this.id = doc.data['id'];
+    this.phoneNumber = doc.data['phoneNumber'];
+    this.fcmDeviceCode = doc.data['fcmDeviceCode'] ?? '';
+    this.pictureUri = doc.data['pictureUri'] ?? '';
+    if (doc.data['cards'] != null)
+      this.cards = List.castFrom(doc.data['cards'] ?? '');
+    else{
+      this.cards=new List();
+    }
+    if (doc.data['couponsIds'] != null)
+      this.couponsIds = List.castFrom(doc.data['couponsIds'] ?? '');
+    else{
+      this.couponsIds=new List();
+    }
+    this.securityPin = doc.data['securityPin'];
+    this.securityQuestion1 = doc.data['securityQuestion1'];
+    this.securityQuestion2 = doc.data['securityQuestion2'];
+    this.securityAnswer1 = doc.data['securityAnswer1'];
+    this.securityAnswer2 = doc.data['securityAnswer2'];
+  }
+}
--- lib/Services/FirebaseAuthService.dart
+++ lib/Services/FirebaseAuthService.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:topup/ModelClasses/UserModel.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -12,11 +13,13 @@ class AuthService {
 
   Future<String> currentUserIdFromAuth() async {
     final FirebaseUser user = await _auth.currentUser();
-    final String uid = user.uid;
-    if (uid.isNotEmpty)
-      return uid;
-    else
-      return "";
+    if (user!=null) {
+      final String uid = user.uid;
+      if (uid.isNotEmpty)
+        return uid;
+     }
+    return "";
+
   }
 
   // auth change user stream
@@ -43,28 +46,57 @@ class AuthService {
       return null;
     }
   }
-  Future updateDatabase(User user) async {
 
-    await DatabaseService(user.id)
-        .setUserData(user);
+  Future<void> verifyPhone(String userPhoneNumber) async {
+    final PhoneVerificationCompleted verified =
+        (AuthCredential authResult) async {
+      _signInWithPhoneNumber(authResult);
+    };
+
+    final PhoneVerificationFailed verificationFailed =
+        (AuthException authException) {
+      print('${authException.message}');
+    };
+
+    final PhoneCodeSent smsSent = (String verId, [int forceResend]) {
+      this.verificationId = verId;
+      setState(() {
+        this.codeSent = true;
+      });
+    };
+
+    final PhoneCodeAutoRetrievalTimeout autoTimeout = (String varId) {
+      this.verificationId = varId;
+    };
+
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: userPhoneNumber,
+        timeout: const Duration(seconds: 50),
+        verificationCompleted: verified,
+        verificationFailed: verificationFailed,
+        codeSent: smsSent,
+        codeAutoRetrievalTimeout: autoTimeout);
   }
-  // register with email and password
-  Future<User> registerWithEmailAndPassword(User newUser) async {
-    try {
-      AuthResult result = await _auth.createUserWithEmailAndPassword(
-          email: newUser.email, password: newUser.password);
-      //todo Checks fails
-      FirebaseUser user = result.user;
-      newUser.id=user.uid;
 
-      // create a new document for the user with the uid
-      await DatabaseService(user.uid)
-          .setUserData(newUser);
-      return newUser;
-    } catch (error) {
-      print(error.toString());
-      return null;
+  void _signInWithPhoneNumber(AuthCredential credential) async {
+    if (credential == null)
+      credential = PhoneAuthProvider.getCredential(
+          verificationId: verificationId, smsCode: enteredCode);
+
+    await FirebaseAuth.instance
+        .signInWithCredential(credential)
+        .then((value) async {
+      if (value != null) {
+        widget.driver.id = value.user.uid;
+        isInfoOk = true;
+      }
+    });
+    if (isInfoOk) {
+      await DatabaseService(widget.driver.id)
+          .updateDriverData(currentUser: widget.driver);
+      gotoSignUp();
     }
+    isInfoOk=false;
   }
 
   // sign out
